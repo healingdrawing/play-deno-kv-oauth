83476aa93a8246b51b3964c3999ee75893bff7a5
healingdrawing <healingdrawing@gmail.com>
2024-06-28 22:55:40 +0300

    show/edit data alive. Double check key-names to key_names

--- ends/data/data.ts
+++ ends/data/data.ts
@@ -20,7 +20,7 @@ app.get("/",
     const provider = await kvdb.get(["oauth2-providers",session_id]).then(entry => entry.value as string | undefined)
     console.log("provider", provider) // google or x. wth, it looks dirty
 
-    const data_json = await kvdb.get(["data", session_id]).then(entry => entry.value as string | undefined)
+    const data_json = await kvdb.get(["data", session_id]).then(entry => entry.value) // as string | undefined)
     let data:Data = data_placeholder
     try {
       data = await data_schema.parseAsync(data_json)
@@ -37,14 +37,52 @@ app.get("/",
 app.post("/",
   async (c) => {
     console.log("we are inside post redirect")
-    return c.redirect("/")
+
+    const session_id = await getSessionId(c.req.raw).then(entry => entry as string | undefined);
+    const is_signed_in = session_id !== undefined; //has session id cookie
+    if (!is_signed_in) { return c.html( await eta.renderAsync("index", {}) ) }
+
+    const body = await c.req.parseBody();
+    console.log("body ", body) // is ok
+
+    let data:Data
+    try{
+      data = await data_schema.parseAsync(body) //bug FAIL place. it was key-name vs key_name
+      console.log("data parsed inside post", data)
+    } catch (e) {
+      console.log("ERROR: parse data from body | ", e, " | session_id ", session_id);
+      data = data_placeholder
+    }
+
+    await kvdb.set(["data", session_id], data)
+
+    return c.redirect("/data")
   }
 )
 
 app.get("/edit",
   async (c) => {
-    console.log("we are inside post redirect")
-    const data = data_placeholder //todo refactor to implement kvdb read
+    console.log("we are inside /edit")
+
+    const session_id = await getSessionId(c.req.raw).then(entry => entry as string | undefined);
+    const is_signed_in = session_id !== undefined; //has session id cookie
+    if (!is_signed_in) { return c.html( await eta.renderAsync("index", {}) ) }
+
+    const raw_data = await kvdb.get(["data", session_id]).then(d => d.value)// as string | undefined)
+
+    let data:Data
+    try{
+      data = await data_schema.parseAsync(raw_data)
+      console.log("data parsed from kvdb inside edit", data)
+    } catch (e) {
+      console.log("ERROR: parse data from kvdb | ", e, " | session_id ", session_id);
+      data = data_placeholder
+    }
+
+    
+
+
+
     return c.html(
       await eta.renderAsync("edit", data? data : {})
     );
