50173c0a9b4ebb23a773101eb36a020b8965341c
healingdrawing <healingdrawing@gmail.com>
2024-06-29 23:58:41 +0300

    wip store profile data

--- oauth2/utils_google.ts
+++ oauth2/utils_google.ts
@@ -1,12 +1,12 @@
-import { createGoogleOAuthConfig } from "https://deno.land/x/deno_kv_oauth@v0.10.0/mod.ts";
+import { createGoogleOAuthConfig } from "https://deno.land/x/deno_kv_oauth@v0.10.0/mod.ts"
 
-import { loadSync, z, kvdb } from "../deps.ts";
-loadSync({ export: true });
+import { loadSync, z, kvdb } from "../deps.ts"
+loadSync({ export: true })
 
 export const google_oauth_config = createGoogleOAuthConfig({
   redirectUri: "http://localhost:8000/callback-google",
   scope: "https://www.googleapis.com/auth/userinfo.profile"
-});
+})
 
 export interface Google_Profile_Data {
   id: string;
@@ -14,7 +14,7 @@ export interface Google_Profile_Data {
   given_name: string;
   family_name: string;
   picture: string;
-};
+}
 
 const google_schema = z.object(
   {
@@ -24,7 +24,7 @@ const google_schema = z.object(
     family_name: z.string(),
     picture: z.string(),
   }
-);
+)
 
 const google_fail_case:Google_Profile_Data = {
   id: "N/A",
@@ -39,12 +39,12 @@ export async function fetch_google_profile_data(access_token: string, session_id
   // todo
   /*
   + inject session_id into function incoming parameters
-  - check if there is record of fetched profile data for session_id
-  - if record of profile data for session_id is present then use data from kvdb
+  + check if there is record of fetched profile data for session_id
+  + if record of profile data for session_id is present then use data from kvdb
   - if NO record, then
-  - - fetch new profile data from google api
-  - - check fetched profile data
-  - - record profile data in kvdb with key equal to ["profile", session_id]
+  + - fetch new profile data from google api
+  + - check fetched profile data
+  + - record profile data in kvdb with key equal to ["profile", session_id]
   - - get the user profile id from fetched data
   - - remove all profile records for user profile id, to prevent garbage in database
   - - finally every new login with new session_id, must remove data from previous fetch
@@ -52,16 +52,34 @@ export async function fetch_google_profile_data(access_token: string, session_id
   */
 
   const profile = await kvdb.get<Google_Profile_Data>(["profile", session_id]).then(d => d.value)
+  if (profile !== null){ return profile }
 
-  const url = "https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=" + access_token;
-  const response = await fetch(url, { method: "GET" });
+  console.log("profile is ", profile, " fetch new data")
 
+  const url = "https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=" + access_token
+  
   try {
-    const data = await response.json();  
-    console.log("data inside fetch google: ", data);
-    return await google_schema.parseAsync(data);    
+    const response = await fetch(url, { method: "GET" })
+    const data_json = await response.json()
+    console.log("data_json inside fetch google: ", data_json)
+    const data = await google_schema.parseAsync(data_json)
+
+    
+    const profiles = kvdb.list<Google_Profile_Data>({prefix: ["profile"]})
+    for await (const x of profiles ){
+      if (x.value.id === data.id){
+        console.log("deleted key", x.key)
+        console.log(x.value)
+        console.log("id ", data.id)
+        kvdb.delete(x.key)
+      }
+    }
+    
+    console.log("new session_id", session_id)
+    await kvdb.set(["profile", session_id], data)
+    return data
   } catch (e) {
-    console.log("ERROR: fetch_google_profile_data | ", e);
+    console.log("ERROR: fetch_google_profile_data | ", e)
   }
   return google_fail_case;  
 }
